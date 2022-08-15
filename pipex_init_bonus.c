59cd52ac8767a3a1d56b43d3f00eb10701e54175
Ngan Tan Wang <ntan-wan@u91z04s02.42kl.edu.my>
2022-08-15 19:05:51 +0800

    norminetted everything 100%

--- pipex_init_bonus.c
+++ pipex_init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 23:45:26 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/08/15 18:42:43 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/08/15 19:02:20 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,20 @@ static void	clear_all(t_pipex_bonus *pipex)
 	print_error_and_exit("envp");
 }
 
+static int	*create_fdstorage(t_pipex_bonus *pipex)
+{
+	int	*fdstorage;
+
+	fdstorage = (int *)malloc(sizeof(int) * pipex->total_pipes_ends);
+	return (fdstorage);
+}
+
 void	pipex_init_bonus(int ac, char **envp, t_pipex_bonus *pipex)
 {
 	pipex->child_index = -1;
 	pipex->total_cmds = ac - 3 - pipex->here_doc;
 	pipex->total_pipes_ends = 2 * (pipex->total_cmds - 1);
-	pipex->pipefd_storage = (int *)malloc(sizeof(int) * pipex->total_pipes_ends);
+	pipex->pipefd_storage = create_fdstorage(pipex);
 	if (!pipex->pipefd_storage)
 		print_error_and_exit("pipe");
 	pipex->env_path = find_envp_path(envp);
