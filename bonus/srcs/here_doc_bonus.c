240f9d534ebefd97104cb9a86a44cfd84453f483
Ngan Tan Wang <ntan-wan@u91z04s03.42kl.edu.my>
2022-08-17 13:31:35 +0800

    fixed limiter issue

--- bonus/srcs/here_doc_bonus.c
+++ bonus/srcs/here_doc_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ntan-wan <ntan-wan@42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 15:35:00 by ntan-wan          #+#    #+#             */
-/*   Updated: 2022/08/17 07:25:31 by ntan-wan         ###   ########.fr       */
+/*   Updated: 2022/08/17 13:29:32 by ntan-wan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,20 @@ int	is_here_doc(char *infile)
 static void	here_doc_process(char *limiter, char **line, int temp_fd)
 {
 	int		limiter_len;
+	char	*limiter_with_null;
 
 	limiter_len = ft_strlen(limiter);
+	limiter_with_null = ft_strjoin(limiter, "\n");
 	while (1)
 	{
 		write(STDOUT, "heredoc> ", 9);
 		*line = get_next_line(STDIN);
-		if (ft_strncmp(limiter, *line, limiter_len) == 0)
+		if (ft_strncmp(limiter_with_null, *line, limiter_len + 1) == 0)
 			break ;
 		write(temp_fd, *line, ft_strlen(*line));
 		free(*line);
 	}	
+	free(limiter_with_null);
 	free(*line);
 	close(temp_fd);
 }
